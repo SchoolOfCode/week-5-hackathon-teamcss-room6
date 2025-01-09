83d433f58c1820bd445829539770c85dfb2ba897
sumeyacodes <sumeya4hmed@gmail.com>
2025-01-09 17:14:20 +0000

    ticket 13 - app.js ready, package.json test & start scripts added

---
+++ app.js
@@ -0,0 +1,17 @@
+// Import the required modules
+import express from "express";
+import morgan from "morgan";
+
+import router from "./routes/journal-entries.js";
+
+// Initialize the express app
+const app = express();
+
+// Middleware
+app.use(morgan("dev")); // Morgan is used for logging HTTP requests to the console in a developer-friendly format
+app.use(express.json()); // express.json() middleware is used to parse incoming JSON requests
+
+// Add sub-routers
+app.use("/", router);
+
+export default app;
