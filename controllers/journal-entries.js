dbaed90f9a1e3c2b2045fa7644b4410afdec72e0
Sergio <sergeh96@gmail.com>
2025-01-09 22:42:41 +0000

    get request controller functions done

--- controllers/journal-entries.js
+++ controllers/journal-entries.js
@@ -1,6 +1,54 @@
-import { fetchAllEntries } from "../models/journal-entries.js";
+import {
+  // importing sumeya's functions
+  fetchAllEntries,
+  fetchEntryByUserId,
+  insertEntry,
+  editByEntryId,
+  removeByEntryId,
+} from "../models/journal-entries.js";
 
-export async function getEntries(req, res) {
-  const data = await fetchAllEntries();
-  res.send(data);
+export async function getAllEntries(req, res) {
+  // Try catch block
+  try {
+    const entries = await fetchAllEntries();
+
+    res.status(200).json({
+      // Updates status code, following Jsend specifications when sending data
+      status: "success",
+      data: entries,
+    });
+  } catch (error) {
+    // Handle errors, send status code, error message in Jsend
+    res.status(500).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+}
+
+export async function getEntryById(req, res) {
+  try {
+    const id = req.params.id;
+    const entry = await fetchEntryByUserId(id);
+
+    res.status(200).json({
+      status: "success",
+      data: entry,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+}
+
+export async function postEntry(req, res) {
+  try {
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      message: error.message,
+    });
+  }
 }
--- routes/journal-entries.js
+++ routes/journal-entries.js
@@ -1,9 +1,9 @@
 import express from "express";
 
-import { getEntries } from "../controllers/journal-entries.js";
+import { getAllEntries } from "../controllers/journal-entries.js";
 
 const router = express.Router();
 
-router.get("/", getEntries); // Test route, need a function
+router.get("/", getAllEntries); // Test route, need a function
 
 export default router;
