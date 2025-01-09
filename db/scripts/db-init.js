efb33356f5595f551c654243aff5be50295f0f4a
Sergio <sergeh96@gmail.com>
2025-01-09 15:22:37 +0000

    first commit, empty files but codebase structure is here

---
+++ db/scripts/db-init.js
@@ -0,0 +1,4 @@
+// TICKET 5
+// Serves the purpose of resetting and seeding the database with dummy data for initial testing
+// Not following convention from the workshop of quering requests directly to the database with SQL commands
+// Instead, creating a seperate commands.sql file that holds the SQL commands, and reading them using the fs package, storing them as a variable, and passing the commands as an argument to the DB query
