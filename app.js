449697728953dc67b722b33faffb6624bf3c6ff7
Sergio <sergeh96@gmail.com>
2025-01-09 18:39:35 +0000

    one working endpoint

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 import express from "express";
 import morgan from "morgan";
 
-import router from "./routes/journal-entries.js";
+import entries from "./routes/journal-entries.js";
 
 // Initialize the express app
 const app = express();
@@ -12,6 +12,6 @@ app.use(morgan("dev")); // Morgan is used for logging HTTP requests to the conso
 app.use(express.json()); // express.json() middleware is used to parse incoming JSON requests
 
 // Add sub-routers
-app.use("/", router);
+app.use("/", entries);
 
 export default app;
